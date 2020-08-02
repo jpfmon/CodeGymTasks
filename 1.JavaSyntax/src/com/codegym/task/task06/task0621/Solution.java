5da010a50ba8bb66830d162c7ad72c3c40a30495
Juan Fdez. Montojo <jpfmontojo@gmail.com>
2020-08-02 23:41:55 +0300

    New task completed

--- 1.JavaSyntax/src/com/codegym/task/task06/task0621/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0621/Solution.java
@@ -13,19 +13,37 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String grandFather = reader.readLine();
+        Cat catGrandFather = new Cat(grandFather);
+
+        String grandMother = reader.readLine();
+        Cat catGrandMother = new Cat(grandMother);
+
+        String fatherName = reader.readLine();
+        Cat catFather = new Cat(fatherName,null,catGrandFather);
+
         String motherName = reader.readLine();
-        Cat catMother = new Cat(motherName);
+        Cat catMother = new Cat(motherName,catGrandMother,null);
+
+        String sonName = reader.readLine();
+        Cat catSon = new Cat(sonName, catMother,catFather);
 
         String daughterName = reader.readLine();
-        Cat catDaughter = new Cat(daughterName, catMother);
+        Cat catDaughter = new Cat(daughterName, catMother,catFather);
 
+
+        System.out.println(catGrandFather);
+        System.out.println(catGrandMother);
+        System.out.println(catFather);
         System.out.println(catMother);
+        System.out.println(catSon);
         System.out.println(catDaughter);
     }
 
     public static class Cat {
         private String name;
         private Cat parent;
+        private Cat secondParent;
 
         Cat(String name) {
             this.name = name;
@@ -36,13 +54,22 @@ public class Solution {
             this.parent = parent;
         }
 
+        public Cat(String name, Cat parent, Cat secondParent) {
+            this.name = name;
+            this.parent = parent;
+            this.secondParent = secondParent;
+        }
+
         @Override
         public String toString() {
-            if (parent == null)
-                return "The cat's name is " + name + ", no mother ";
+            if (parent == null && secondParent == null)
+                return "The cat's name is " + name + ", no mother, no father";
+            else if (parent !=null && secondParent == null)
+                return "The cat's name is " + name + ", " + parent.name + " is the mother, no father";
+            else if (parent == null && secondParent !=null)
+                return "The cat's name is " + name + ", no mother, " + secondParent.name + " is the father";
             else
-                return "The cat's name is " + name + ", " + parent.name + " is the mother";
-        }
+                return "The cat's name is " + name + ", " + parent.name + " is the mother" + ", " + secondParent.name + " is the father";        }
     }
 
 }
