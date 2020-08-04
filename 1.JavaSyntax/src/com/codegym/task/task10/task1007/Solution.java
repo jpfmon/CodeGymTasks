1fcb7bb554abeebd273d8d6425fac0fd046816a4
Juan Fdez. Montojo <jpfmontojo@gmail.com>
2020-08-04 19:38:50 +0300

    Updating

--- 1.JavaSyntax/src/com/codegym/task/task10/task1007/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1007/Solution.java
@@ -7,10 +7,18 @@ Task No. 7 about integer type conversions
 
 public class Solution {
     public static void main(String[] args) {
-        long l = (byte) 1234_564_890L;
-        int x = (byte) 0b1000_1100_1010;
+//        long l = (byte) 1234_564_890L;
+        long l =  1234_564_890L;
+//        System.out.println(l);
+//        int x = (byte) 0b1000_1100_1010;
+        int x =  0b1000_1100_1010;
+//        System.out.println(x);
         double m = (byte) 110_987_654_6299.123_34;
-        float f = (byte) l++ + 10 + ++x - (float) m;
+//        System.out.println(m);
+//        float f = (byte) l++ + 10 + ++x - (float) m;
+        float f = l++ + 10 + ++x - (float) m;
+
+//        System.out.println(f);
         l = (long) f / 1000;
         System.out.println(l);
     }
--- 1.JavaSyntax/src/com/codegym/task/task10/task1019/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1019/Solution.java
@@ -1,6 +1,9 @@
 package com.codegym.task.task10.task1019;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 /* 
 Functionality is not enough!
@@ -11,9 +14,34 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int id = Integer.parseInt(reader.readLine());
-        String name = reader.readLine();
+        int id = 0;
+        String name = null;
 
-        System.out.println("Id=" + id + " Name=" + name);
+
+
+        boolean next = true;
+
+        HashMap<String, Integer> map = new HashMap();
+
+
+
+        while (true) {
+
+            try {
+                id = Integer.parseInt(reader.readLine());
+            } catch (Exception e) {
+
+                break;
+            }
+
+            name = reader.readLine();
+
+            map.put(name, id);
+            if(name.isEmpty()){
+                break;
+            }
+        }
+
+        map.forEach((key, value) -> System.out.println( value + " " + key));
     }
 }
